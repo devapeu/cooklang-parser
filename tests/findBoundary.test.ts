910e9bccc114621139ed7db4244e4c2d1cb107af
Piero del Valle <devapeu@gmail.com>
2025-11-15 18:00:23 -0500

    fix: add more tests for boundary

--- tests/findBoundary.test.ts
+++ tests/findBoundary.test.ts
@@ -2,9 +2,9 @@ import { describe, expect, test } from "@jest/globals";
 import { findBoundary } from "../app";
 
 describe('findBoundary', () => {
-  test('reads line until next { when both a space and {} are present', () => {
-    let line = "@green onions{5}";
-    expect(findBoundary(1, line)).toBe(13);
+  test('reads line until end of string', () => {
+    let line = "@salt";
+    expect(findBoundary(1, line)).toBe(5);
   });
 
   test('reads line until {', () => {
@@ -17,8 +17,23 @@ describe('findBoundary', () => {
     expect(findBoundary(1, line)).toBe(5);
   });
 
-  test('reads line until end of string', () => {
-    let line = "@salt";
-    expect(findBoundary(1, line)).toBe(5);
+  test('reads line until next { when both a space and {} are present', () => {
+    let line = "@green onions{1%bunch}";
+    expect(findBoundary(1, line)).toBe(13);
   });
+
+    test('reads first match only', () => {
+    let line = "@salt with @onions{3}";
+    expect(findBoundary(1, line)).toBe(5);
+  })
+
+  test('reads first match only with quantity', () => {
+    let line = "@onions{3} and @garlic{2%cloves}";
+    expect(findBoundary(1, line)).toBe(7);
+  })
+
+  test('reads first match only with quantity, when it has spaces', () => {
+    let line = "@green onions{3} and @garlic{2%cloves}";
+    expect(findBoundary(1, line)).toBe(7);
+  })
 })
